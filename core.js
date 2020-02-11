7a31d4290a3f986b880885865194dc3b43d8926e
mahmoodSherif <ma13121999@gmail.com>
2020-02-11 23:10:01 +0200

    add id attribute and change library to dictionary

--- core.js
+++ core.js
@@ -1,13 +1,18 @@
-let library = []
+let library = {};
+let libraryId = 0;
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = libraryId++;
 }
 Book.prototype.info = ()=>{
     return `${this.title} by ${this.author}, ${this.pages} pages , ${(this.read)?"readed":"not read yet"}`;
 };
 function addBookToLibrary(book){
-    library.push(book);
+    library[book.id] = book;
+}
+function removeBookFormLibrary(id) {
+    delete library[id];
 }
